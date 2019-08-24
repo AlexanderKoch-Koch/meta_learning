99f54fcc4cef6b401552016813f8e38a90374721
Alexander Koch <alexander@4koch.de>
2019-08-24 11:49:01 +0200

    add tests with static weights

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tensorflow.keras as keras
 from datetime import datetime
 
 
-inputs = keras.layers.Input(batch_shape=(1, 1, 4))
+inputs = keras.layers.Input(batch_shape=(1, 1, 3))
 x = keras.layers.Dense(6)(inputs)
 x = keras.layers.LSTM(8, stateful=True)(x)
 policy_output = keras.layers.Dense(2, activation='softmax' )(x)
@@ -15,19 +15,21 @@ outputs = keras.layers.concatenate(inputs=[policy_output, critic_output])
 model = keras.Model(inputs=inputs, outputs=outputs)
 
 optimizer = keras.optimizers.Adam(learning_rate=0.001)
+# optimizer = tf.keras.optimizers.RMSprop(learning_rate=7e-4, epsilon=0.1, decay=0.99)
+# optimizer = keras.optimizers.SGD(lr=0.01, decay=0.0001)
 gamma = 0
 
 def main():
     r = 0
     a = 0
     
-    for episode in range(10000):
+    for episode in range(500):
         reward_output = random.randint(0,1)
         model.reset_states()
-        for step in range(100):
-            neural_net_input = tf.concat([tf.one_hot(a, depth=2) ,  [r, step]], 0)
+        for step in range(5):
+            neural_net_input = [a, r, step]
             model_input = np.expand_dims(np.expand_dims(neural_net_input, axis=0), axis=0).astype('float32')
-            step = episode * 100 + step
+            step = episode * 5 + step
             
             # single batch single time step
             with tf.GradientTape() as tape:
@@ -51,7 +53,25 @@ def main():
             tf.summary.scalar('output_0', output[0], step)
             tf.summary.scalar('output_1', output[1], step)
 
+    reward_sum = [0, 0, 0, 0, 0] 
+    for test in range(50):
+        a = 0
+        r = 0
+        reward_output = random.randint(0, 1)
+        for step in range(5):
 
+            neural_net_input = [a, r, step]
+            model_input = np.expand_dims(np.expand_dims(neural_net_input, axis=0), axis=0).astype('float32')
+
+            output = model(model_input)
+            a = np.argmax(output[0:2])
+            r = 1 if a == reward_output else 0
+            reward_sum[step] += r
+
+    print(reward_sum)
+    mean_reward = sum(reward_sum) /250.0
+    print("ANN achieved mean reward of: " + str(mean_reward))
+    print("perfect score is: " + str(225/250.0))
 
 if __name__ == '__main__':
     log_dir = "./runs/" + datetime.now().strftime("%d.%m_%H:%M:%S")
